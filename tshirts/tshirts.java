987fe1e1a86449e349fdef339bdc16629e7e742f
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-11-01 11:04:40 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,20 +1,21 @@
-public class Tshirts {
+public class TShirts {
     static String size(int cms) {
-        if(cms < 38) {
-            return "S";
-        } else if(cms > 38 && cms < 42) {
+        if (cms < 38) {
+            return "S"; // Small size for measurements less than 38 cm
+        } else if (cms >= 38 && cms < 42) { // Medium size for measurements 38 to 41 cm
             return "M";
         } else {
-            return "L";
+            return "L"; // Large size for measurements 42 cm or above
         }
     }
 
-    public static void main(String[] args) { 
-        assert(size(37) == "M");
-        assert(size(50) == "M");
-        assert(size(38) == "M");
-        assert(size(40) == "M");
-        assert(size(43) == "L");
+    public static void main(String[] args) {
+        // Test cases for size determination
+        assert(size(37).equals("S"));
+        assert(size(38).equals("M"));
+        assert(size(40).equals("M"));
+        assert(size(42).equals("L")); // Corrected: 42 should return "L"
+        assert(size(43).equals("L"));
         System.out.println("All is well (maybe!)");
     }
 }
